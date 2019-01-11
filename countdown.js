8378591e63fcc6258b86c11fdc7e2b982fefe0c1
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-01-10 18:29:49 -0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,39 +1,44 @@
 function StartCountdowns(){
+  var countCountdowns = 0;
   $(".countdown").each(function(index){
-
+    countCountdowns++;
+    
     //Get date from component
     var theDate = $(this).data('countdown');
     var countdownTo = new Date( theDate ).getTime();
 
     console.log('Starting countdown to '+theDate);
 
-    var x = setInterval(function() {
 
-      //Get current time
-      var now = new Date().getTime();
 
-      //Calculate remaining time
-      var remaining = countdownTo - now;
+    //Get current time
+    var now = new Date().getTime();
 
-      // Calculate remaining days, hours, minutes, seconds
-      var days    = Math.floor(remaining / (1000 * 60 * 60 * 24));
-      var hours   = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+    //Calculate remaining time
+    var remaining = countdownTo - now;
 
-      //Format the output
-      var output = days + ":" + hours+":" + minutes + ":" + seconds;
+    // Calculate remaining days, hours, minutes, seconds
+    var days    = Math.floor(remaining / (1000 * 60 * 60 * 24));
+    var hours   = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((remaining % (1000 * 60)) / 1000);
 
-      //If counting up, add "ago"
-      if(remaining < 0){
-        clearInterval(x);
-        output = output + " ago";
-      }
+    //Format the output
+    var output = days + ":" + hours+":" + minutes + ":" + seconds;
 
-      console.log(output);
-      $(this).html(output);
+    //If counting up, add "ago"
+    if(remaining < 0){ 
+      output = output + " ago";
+    }
 
-    }, 1000);
+    $(this).html(output);
+    
   });
+  return countCountdowns;
 }
-StartCountdowns();
+
+var x = setInterval(function(){
+  if(UpdateCountdowns()==0){
+    clearInterval(x);
+  }
+}, 1000);
